7380b5aa30cbe0a985f5eab5719807b72d8c0448
MikeYoulend <mikeyoulend@gmail.com>
2023-10-23 14:06:35 +0200

    Final

--- src/fe/pages/AdminPage.jsx
+++ src/fe/pages/AdminPage.jsx
@@ -12,7 +12,11 @@ const AdminPage = () => {
 				<Navbar.Toggle aria-controls="basic-navbar-nav" />
 				<Navbar.Collapse id="basic-navbar-nav">
 					<Nav className="ml-auto">
-						<Nav.Link className="ms-2 fs-1 " as={Link} to="/home">
+						<Nav.Link
+							className="ms-2 fs-1 "
+							as={Link}
+							to={`/home/${localStorage.getItem("token")}`}
+						>
 							Home
 						</Nav.Link>
 						{/* Aggiungi altri link per le tue pagine qui */}
